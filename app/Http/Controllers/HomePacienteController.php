25e629d499b8dac01eb50bc47a730e816c5cb539
cdmm299 <carlosmendizabal299@gmail.com>
2023-09-20 19:45:27 -0400

    implementacion parcial de rf10

--- app/Http/Controllers/HomePacienteController.php
+++ app/Http/Controllers/HomePacienteController.php
@@ -15,19 +15,64 @@ class HomePacienteController extends Controller
 {
   public function index()
   {
-    //$users = User::all()->except(Auth::id());
-    return Inertia::render('HomePaciente' /*, [
-      'users' => $users,
-    ]*/);
+    $user = Auth::user();
+    Log::info('usuario');
+    Log::info($user);
+
+    $obj = DB::table('pacientes')
+      ->where('pacientes.user_id', Auth::id())
+      ->get();
+
+    Log::info('obj');
+    Log::info($obj);
+
+    $sesiones = DB::table('sesions')
+      ->where('sesions.paciente_id', $obj->first()->id)
+      ->orderBy('sesions.updated_at', 'desc')
+      ->get();
+
+    Log::info('sesiones');
+    Log::info($sesiones);
+
+    //conectar cv desde tabla archivos
+    if ($obj->first()->psicologo_id != null) {
+      $psicologos = DB::table('users')
+        ->selectRaw(
+          'psicologos.id, name, apellidos,profile_photo_path,fecha_nacimiento,fecha_funcion_titulo,universidad,ciudad_residencia,departamento_residencia,pais_residencia,descripcion_cv,foto'
+        )
+        ->join('psicologos', 'users.id', '=', 'psicologos.user_id')
+        ->where('psicologos.estado', 'activo')
+        ->get();
+    } else {
+      $psicologos = [];
+    }
+
+    return Inertia::render('HomePaciente', [
+      'user' => $user,
+      'psicologo_id' => $obj->first()->psicologo_id,
+      'paciente_id' => $obj->first()->id,
+      'sesiones' => $sesiones,
+      'psicologos' => $psicologos,
+    ]);
     //  Log::info('LOG EXAMPLE');
   }
 
-  /*public function update(Request $request, $id)
+  //funcion para pago de sesion cancelada
+  public function store(Request $request)
   {
-    $user = User::find($id);
+  }
+
+  //programar sesion
+  public function update(Request $request, $id)
+  {
+    //if request psicologo distinto de ""
+    //asignarpsicologo
+    //programar sesion
+
+    /* $user = User::find($id);
     $user->contador_bloqueos = 0;
     $user->bloqueo_permanente = false;
     $user->fill($request->input())->saveOrFail();
-    return redirect('usuarios');
-  }*/
+    return redirect('usuarios');*/
+  }
 }
--- resources/js/Pages/HomePaciente.tsx
+++ resources/js/Pages/HomePaciente.tsx
@@ -1,9 +1,154 @@
-import React from 'react';
+import React, { useRef, useState } from 'react'
 import AppLayout from '@/Layouts/AppLayout';
 import LogoLayout from '@/Layouts/LogoLayout';
+import TablaUsuarios from '@/Components/TablaUsuarios';
 import Titulo from '@/Components/Titulo';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { useForm } from '@inertiajs/react';
+import InputError from '@/Components/InputError';
+import useRoute from '@/Hooks/useRoute';
+import CustomButton from '@/Components/CustomButton';
+import InputLabel from '@/Components/InputLabel';
 
-export default function HomePaciente() {
+interface Props{
+    user:JSON;
+    psicologo_id:number;
+    paciente_id:number;
+    sesiones:any;
+    psicologos:Array<JSON>;
+}
+
+/*interface User{
+  id: string
+  name: string
+  apellidos: string
+  email: string
+  canal_comunicacion: string
+  fecha_nacimiento: Date
+  ocupacion: string
+  ci: string
+}*/
+
+export default function HomePaciente({user,psicologo_id,paciente_id,sesiones,psicologos}:Props) {
+    console.log("user")
+    console.log(user)
+    console.log("psicologo_id")
+    console.log(psicologo_id)
+    console.log("paciente_id")
+    console.log(paciente_id)
+    console.log("sesiones")
+    console.log(sesiones)
+    console.log("ultima sesion id")
+    console.log(sesiones[0].id)
+    console.log("psicologos")
+    console.log(psicologos)
+
+    const route = useRoute();
+  const [switchVisibility, setSwitchVisibility] = useState("");
+  const fecha_hora_inicioInput= useRef(null)
+  const fecha_hora_finInput= useRef(null)
+
+  const servicioInput= useRef(null)
+  const institucionInput= useRef(null)
+  const convenioInput= useRef(null)
+  const{data,setData,put,post,processing,reset,errors} = useForm({
+    estado:'pendiente',
+    pago_confirmado:0,
+    fecha_hora_inicio:'',
+    fecha_hora_fin:'',
+    paciente_id:paciente_id,
+    psicologo_id:psicologo_id,
+    //
+    servicio:'',
+    institucion:'',
+    convenio:'',
+    sesion_id:sesiones[0].id
+  })
+  //check si hay una sesion programada
+  //si hay no mostrar nada
+  //si no hay mostrar el boton de programacion
+
+  //cuando se apreta el boton
+  //si el usuario debe compensar una sesion y tiene una solicitud pendiente mostrar alerta de que espere
+  //si el usuario debe compensar una sesion y no tiene solicitud pendiente mostrar form de pago y mandar solicitud
+  
+  //si el usuario tiene todos los pagos confirmados->
+  //si el usuario no tiene psicologo elegir uno->asignar
+  //cuando el usuario tiene psicologo puede finalmente agendar sesion mostrando calendario
+
+  const mostrarFormSesion = (obj: any) => {
+    setSwitchVisibility("formSesion");
+    setData({
+      fecha_hora_inicio:obj.fecha_hora_inicio,
+      fecha_hora_fin:obj.fecha_hora_fin
+});
+  };
+
+  const ocultarFormSesion = () => {
+    setSwitchVisibility("");
+  };
+
+  const mostrarFormPsicologo = (obj: any) => {
+    setSwitchVisibility("formPsicologo");
+    /*setData({
+
+});*/
+  };
+
+  const ocultarFormPsicologo = () => {
+    setSwitchVisibility("");
+  };
+
+  const mostrarFormPago = (obj: any) => {
+    setSwitchVisibility("formPago");
+    setData({
+      servicio:obj.servicio,
+      institucion:obj.institucion,
+      convenio:obj.convenio,
+});
+  };
+
+  const ocultarFormPago = () => {
+    setSwitchVisibility("");
+  };
+
+  const update = (e:any) => {
+    e.preventDefault();
+    setSwitchVisibility("");
+    put(route('homePaciente.update',data.id),{
+      onSuccess:()=>{
+        alert("Exito")
+      },
+      onError:()=>{
+        /*if(errors.name){
+          reset('name')
+        }
+        if(errors.email){
+          reset('email')
+        }*/
+      },
+    });
+  };
+
+  const save = (e:any) => {
+    e.preventDefault();
+    //setSwitchVisibility(true);
+    post(route('homePaciente.store'),{
+      onSuccess:()=>{
+        alert("Exito")
+      },
+      onError:()=>{
+        /*if(errors.name){
+          reset('name')
+        }
+        if(errors.email){
+          reset('email')
+        }*/
+      },
+    });
+    
+  };
     return (
       <LogoLayout>
         <AppLayout
@@ -12,8 +157,180 @@ export default function HomePaciente() {
           <br/>
             <div className={` min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900`}>
               <Titulo>Home Paciente</Titulo>
-              </div>
+
+              <br />
+             <form className="w-[350px]" onSubmit={save}>
+              <div className="mt-4"> 
+                <InputLabel htmlFor="servicio">Servicio</InputLabel>
+                  <TextInput
+                    id="servicio"
+                    name="servicio"
+                    ref={servicioInput}
+                    className="mt-1 block w-full isFocused "
+                    value={data.servicio}
+                  onChange={e => setData('servicio', e.target.value)}
+                    required
+                    
+                    placeholder="servicio"
+                  />
+                  <InputError className="mt-2" message={errors.servicio} />
+                </div>
+                <br />
+
+                <div className="mt-4"> 
+                <InputLabel htmlFor="institucion">Institución</InputLabel>
+                  <TextInput
+                    id="institucion"
+                    name="institucion"
+                    ref={institucionInput}
+                    className="mt-1 block w-full isFocused "
+                    value={data.institucion}
+                  onChange={e => setData('institucion', e.target.value)}
+                    required
+                    
+                    placeholder="institucion"
+                  />
+                  <InputError className="mt-2" message={errors.institucion} />
+                </div>
+                <br />
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="convenio">Convenio</InputLabel>
+                    <TextInput
+                      id="convenio"
+                      name="convenio"
+                      ref={convenioInput}
+                      className="mt-1 block w-full "
+                      value={data.convenio}
+                    onChange={e => setData('convenio', e.target.value)}
+                      required
+                      
+                      placeholder="convenio"
+                    />
+                    <InputError className="mt-2" message={errors.convenio} />
+                  </div>
+                <br />
+               
+                {/*  <label>
+                    Checkbox: <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+                  </label>
+                  <hr />
+                  <p>
+                    Radio buttons:
+                    <label><input type="radio" name="myRadio" value="option1" /> Option 1</label>
+                    <label><input type="radio" name="myRadio" value="option2" defaultChecked={true} /> Option 2</label>
+                    <label><input type="radio" name="myRadio" value="option3" /> Option 3</label>
+                  </p>
+                  <hr />
+                  <button type="reset">Reset form</button>
+                  <button type="submit">Submit form</button>
+                      */}
+
+                <CustomButton onClick={()=>save}>Editar usuario</CustomButton>
+              </form>
               <br/>
+              <PrimaryButton   onClick={()=>ocultarFormPago()}>Cancelar</PrimaryButton>
+
+
+              <br />
+             <form className="w-[350px]" onSubmit={update}>
+              <div className="mt-4"> 
+                <InputLabel htmlFor="fecha_hora_inicio">Fecha Hora Inicio</InputLabel>
+                  <TextInput
+                    id="fecha_hora_inicio"
+                    name="fecha_hora_inicio"
+                    ref={fecha_hora_inicioInput}
+                    className="mt-1 block w-full isFocused "
+                    value={data.fecha_hora_inicio}
+                  onChange={e => setData('fecha_hora_inicio', e.target.value)}
+                    required
+                    
+                    placeholder="fecha hora inicio"
+                  />
+                  <InputError className="mt-2" message={errors.fecha_hora_inicio} />
+                </div>
+                <br />
+
+                <div className="mt-4"> 
+                <InputLabel htmlFor="fecha_hora_fin">Fecha Hora Fin</InputLabel>
+                  <TextInput
+                    id="fecha_hora_fin"
+                    name="fecha_hora_fin"
+                    ref={fecha_hora_finInput}
+                    className="mt-1 block w-full isFocused "
+                    value={data.fecha_hora_fin}
+                  onChange={e => setData('fecha_hora_fin', e.target.value)}
+                    required
+                    
+                    placeholder="fecha hora fin"
+                  />
+                  <InputError className="mt-2" message={errors.fecha_hora_fin} />
+                </div>
+                <br />
+               
+                {/*  <label>
+                    Checkbox: <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+                  </label>
+                  <hr />
+                  <p>
+                    Radio buttons:
+                    <label><input type="radio" name="myRadio" value="option1" /> Option 1</label>
+                    <label><input type="radio" name="myRadio" value="option2" defaultChecked={true} /> Option 2</label>
+                    <label><input type="radio" name="myRadio" value="option3" /> Option 3</label>
+                  </p>
+                  <hr />
+                  <button type="reset">Reset form</button>
+                  <button type="submit">Submit form</button>
+                      */}
+
+                <CustomButton onClick={()=>update}>Editar usuario</CustomButton>
+              </form>
+              <br/>
+              <PrimaryButton   onClick={()=>ocultarFormSesion()}>Cancelar</PrimaryButton>
+
+              
+              <br/>
+
+              <table className="table-auto">
+
+                <thead>
+                <tr>
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>Fecha de nacimiento</th>
+                <th>Fecha de función de titulo</th>
+                <th>Universidad</th>
+                {/*<th>Ciudad de residencia</th>
+                <th>Departamento de residencia</th>
+                    <th>País de residencia</th>*/}
+                <th>CV</th>
+                <th>Descripción de CV</th>
+                <th>Foto</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                {psicologos.map((item:any) => (
+                <tr  key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.apellidos}</td>
+                <td>{item.fecha_nacimiento}</td>
+                <td>{item.fecha_funcion_titulo}</td>
+                <td>{item.universidad}</td>
+                {/*<td>{item.ciudad_residencia}</td>
+                <td>{item.departamento_residencia}</td>
+                <td>{item.pais_residencia}</td>*/}
+                <td>{item.cv}</td>
+                <td>{item.descripcion_cv}</td>
+                <td>{item.foto}</td>
+                <td><PrimaryButton onClick={()=>onClickItem(item)}>Elegir psicólogo</PrimaryButton></td>
+
+                </tr>
+                    
+                ))}
+                </tbody>
+              </table>
+              </div>
         </AppLayout>
         </LogoLayout>
       );
